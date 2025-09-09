69297a8599c5a60d1d0a1619e2f7e660c1058b0e
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2025-09-09 15:12:18 +0200

    plus triangel

--- triangel.py
+++ triangel.py
@@ -2,9 +2,8 @@ import math
 a=float(input("Enter side a: "))
 b=float(input("Enter side b: "))
 c=float(input("Enter side c: "))
-if a+b > c and a+c > b and b+c > a:
+if a+b+c==180:
     print("This is a valid triangle")
     
 else:
         print("This is not a valid triangle")
-   
